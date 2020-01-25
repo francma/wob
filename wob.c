c54cee8661304f24e95fceb1ef00e8d155a7889f
Martin Franc <me@martinfranc.eu>
2020-01-25 23:02:44 +0100

    Show bar on all available outputs

--- wob.c
+++ wob.c
@@ -55,19 +55,24 @@ struct wob_geom {
 	unsigned long margin;
 };
 
+struct wob_output {
+	struct wl_list link;
+	struct wl_output *wl_output;
+	struct wl_surface *wl_surface;
+	struct zwlr_layer_surface_v1 *zwlr_layer_surface;
+};
+
 struct wob {
+	int shmid;
 	struct wl_buffer *wl_buffer;
 	struct wl_compositor *wl_compositor;
 	struct wl_display *wl_display;
-	struct wl_output *wl_output;
+	struct wl_list wob_outputs;
 	struct wl_registry *wl_registry;
 	struct wl_shm *wl_shm;
-	struct wl_surface *wl_surface;
+	struct wob_geom *wob_geom;
 	struct xdg_wm_base *xdg_wm_base;
 	struct zwlr_layer_shell_v1 *zwlr_layer_shell;
-	struct zwlr_layer_surface_v1 *zwlr_layer_surface;
-	struct wob_geom *wob_geom;
-	int shmid;
 };
 
 unsigned long
@@ -107,9 +112,9 @@ handle_global(void *data, struct wl_registry *registry, uint32_t name, const cha
 		app->xdg_wm_base = wl_registry_bind(registry, name, &xdg_wm_base_interface, 1);
 	}
 	else if (strcmp(interface, "wl_output") == 0) {
-		if (!app->wl_output) {
-			app->wl_output = wl_registry_bind(registry, name, &wl_output_interface, 1);
-		}
+		struct wob_output *output = calloc(1, sizeof(struct wob_output));
+		output->wl_output = wl_registry_bind(registry, name, &wl_output_interface, 1);
+		wl_list_insert(&app->wob_outputs, &output->link);
 	}
 	else if (strcmp(interface, zwlr_layer_shell_v1_interface.name) == 0) {
 		app->zwlr_layer_shell = wl_registry_bind(registry, name, &zwlr_layer_shell_v1_interface, 1);
@@ -175,6 +180,8 @@ wob_create_surface(struct wob *app)
 		.closed = layer_surface_closed,
 	};
 
+	wl_list_init(&app->wob_outputs);
+
 	app->wl_registry = wl_display_get_registry(app->wl_display);
 	if (app->wl_registry == NULL) {
 		fprintf(stderr, "wl_display_get_registry failed\n");
@@ -201,23 +208,26 @@ wob_create_surface(struct wob *app)
 		exit(EXIT_FAILURE);
 	}
 
-	app->wl_surface = wl_compositor_create_surface(app->wl_compositor);
-	if (app->wl_surface == NULL) {
-		fprintf(stderr, "wl_compositor_create_surface failed\n");
-		exit(EXIT_FAILURE);
-	}
-	app->zwlr_layer_surface = zwlr_layer_shell_v1_get_layer_surface(app->zwlr_layer_shell, app->wl_surface, app->wl_output, ZWLR_LAYER_SHELL_V1_LAYER_OVERLAY, "wob");
-	if (app->zwlr_layer_surface == NULL) {
-		fprintf(stderr, "wl_compositor_create_surface failed\n");
-		exit(EXIT_FAILURE);
+	struct wob_output *output, *tmp;
+	wl_list_for_each_safe (output, tmp, &app->wob_outputs, link) {
+		output->wl_surface = wl_compositor_create_surface(app->wl_compositor);
+		if (output->wl_surface == NULL) {
+			fprintf(stderr, "wl_compositor_create_surface failed\n");
+			exit(EXIT_FAILURE);
+		}
+
+		output->zwlr_layer_surface = zwlr_layer_shell_v1_get_layer_surface(app->zwlr_layer_shell, output->wl_surface, output->wl_output, ZWLR_LAYER_SHELL_V1_LAYER_OVERLAY, "wob");
+		if (output->zwlr_layer_surface == NULL) {
+			fprintf(stderr, "wl_compositor_create_surface failed\n");
+			exit(EXIT_FAILURE);
+		}
+		zwlr_layer_surface_v1_set_size(output->zwlr_layer_surface, app->wob_geom->width, app->wob_geom->height);
+		zwlr_layer_surface_v1_set_anchor(output->zwlr_layer_surface, app->wob_geom->anchor);
+		zwlr_layer_surface_v1_set_margin(output->zwlr_layer_surface, app->wob_geom->margin, app->wob_geom->margin, app->wob_geom->margin, app->wob_geom->margin);
+		zwlr_layer_surface_v1_add_listener(output->zwlr_layer_surface, &zwlr_layer_surface_listener, output);
+		wl_surface_commit(output->wl_surface);
 	}
 
-	zwlr_layer_surface_v1_set_size(app->zwlr_layer_surface, app->wob_geom->width, app->wob_geom->height);
-	zwlr_layer_surface_v1_set_anchor(app->zwlr_layer_surface, app->wob_geom->anchor);
-	zwlr_layer_surface_v1_set_margin(app->zwlr_layer_surface, app->wob_geom->margin, app->wob_geom->margin, app->wob_geom->margin, app->wob_geom->margin);
-	zwlr_layer_surface_v1_add_listener(app->zwlr_layer_surface, &zwlr_layer_surface_listener, app->zwlr_layer_surface);
-
-	wl_surface_commit(app->wl_surface);
 	if (wl_display_roundtrip(app->wl_display) == -1) {
 		fprintf(stderr, "wl_display_roundtrip failed\n");
 		exit(EXIT_FAILURE);
@@ -227,9 +237,13 @@ wob_create_surface(struct wob *app)
 void
 wob_flush(struct wob *app)
 {
-	wl_surface_attach(app->wl_surface, app->wl_buffer, 0, 0);
-	wl_surface_damage(app->wl_surface, 0, 0, app->wob_geom->width, app->wob_geom->height);
-	wl_surface_commit(app->wl_surface);
+	struct wob_output *output, *tmp;
+	wl_list_for_each_safe (output, tmp, &app->wob_outputs, link) {
+		wl_surface_attach(output->wl_surface, app->wl_buffer, 0, 0);
+		wl_surface_damage(output->wl_surface, 0, 0, app->wob_geom->width, app->wob_geom->height);
+		wl_surface_commit(output->wl_surface);
+	}
+
 	if (wl_display_dispatch(app->wl_display) == -1) {
 		fprintf(stderr, "wl_display_dispatch failed\n");
 		exit(EXIT_FAILURE);
@@ -243,25 +257,28 @@ wob_destroy_surface(struct wob *app)
 		return;
 	}
 
-	zwlr_layer_surface_v1_destroy(app->zwlr_layer_surface);
 	zwlr_layer_shell_v1_destroy(app->zwlr_layer_shell);
-	wl_surface_destroy(app->wl_surface);
 	wl_registry_destroy(app->wl_registry);
 	xdg_wm_base_destroy(app->xdg_wm_base);
 	wl_buffer_destroy(app->wl_buffer);
 	wl_compositor_destroy(app->wl_compositor);
 	wl_shm_destroy(app->wl_shm);
-	wl_output_destroy(app->wl_output);
 
+	app->zwlr_layer_shell = NULL;
+	app->wl_registry = NULL;
+	app->xdg_wm_base = NULL;
 	app->wl_buffer = NULL;
 	app->wl_compositor = NULL;
-	app->wl_output = NULL;
-	app->wl_registry = NULL;
 	app->wl_shm = NULL;
-	app->wl_surface = NULL;
-	app->xdg_wm_base = NULL;
-	app->zwlr_layer_shell = NULL;
-	app->zwlr_layer_surface = NULL;
+
+	struct wob_output *output, *tmp;
+	wl_list_for_each_safe (output, tmp, &app->wob_outputs, link) {
+		zwlr_layer_surface_v1_destroy(output->zwlr_layer_surface);
+		wl_output_destroy(output->wl_output);
+		wl_surface_destroy(output->wl_surface);
+
+		free(output);
+	}
 
 	if (wl_display_roundtrip(app->wl_display) == -1) {
 		fprintf(stderr, "wl_display_roundtrip failed\n");
@@ -661,13 +678,11 @@ main(int argc, char **argv)
 
 					assert(app.wl_buffer);
 					assert(app.wl_compositor);
-					assert(app.wl_output);
 					assert(app.wl_registry);
 					assert(app.wl_shm);
-					assert(app.wl_surface);
 					assert(app.xdg_wm_base);
 					assert(app.zwlr_layer_shell);
-					assert(app.zwlr_layer_surface);
+					assert(&app.wob_outputs);
 				}
 
 				if (old_background_color != background_color || old_border_color != border_color) {
